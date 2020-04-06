b6e5bad14d65f9a91a37dfaded876894aa9ed012
deepinder12 <deepinder93@gmail.com>
2020-04-06 11:10:23 -0700

    bash

--- bash.sh
+++ bash.sh
@@ -1,5 +1,20 @@
 cat templates/top.html content/index.html templates/bottom.html
 > docs/index.html
 
+cat index.html > content/index.html
+
 cat about.html > content/about.html
+
+cat contact.html > content/contact.html
+
+cat projects.html > content/projects.html
+
+cat index.html > docs/index.html
+
+cat about.html > docs/about.html
+
+cat contact.html > docs/contact.html
+
+cat projects.html > docs/projects.html
+
 
